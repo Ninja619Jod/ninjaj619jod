98f27921cd6ac4d293b7d2e23dbc9aed8a5cd9b2
niraj <niraj@DESKTOP-944TSMG>
2023-02-14 00:25:37 +0530

    [Ninja]:Ram

---
+++ src/firstPackage/ShreRam.java
@@ -0,0 +1,11 @@
+package firstPackage;
+
+public class ShreRam {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Jai Shree Ram...");
+		System.out.println("Jai Hanuman...");
+		System.out.println("Om Shree Ganeshay Namah...");
+	}
+}
